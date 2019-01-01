5dba53cf0d822daed4cc201a4780650fa51fba23
Austin <12504656+Austin-Scott@users.noreply.github.com>
2019-01-01 14:06:27 -0600

    Find the distance between two coordinates

--- HeatMap/HeatMap/GeographicCoordinate.cpp
+++ HeatMap/HeatMap/GeographicCoordinate.cpp
@@ -7,12 +7,14 @@ using namespace std;
 
 GeographicCoordinate::GeographicCoordinate()
 {
+	isSet = false;
 	latitude = 0.0;
 	longitude = 0.0;
 }
 
 GeographicCoordinate::GeographicCoordinate(double latitude, double longitude)
 {
+	isSet = true;
 	this->latitude = latitude;
 	this->longitude = longitude;
 }
@@ -31,6 +33,24 @@ string GeographicCoordinate::toString()
 {
 	string result = "Latitude: " + to_string(latitude) + " Longitude: " + to_string(longitude);
 	return result;
+}
+
+bool GeographicCoordinate::getIsSet()
+{
+	return isSet;
+}
+
+double GeographicCoordinate::getKilometersTo(GeographicCoordinate other)
+{
+	if (this->latitude==other.getLat() && this->longitude==other.getLon())
+		return 0.0;
+
+	const double radius = 6371.0; //Average radius of Earth
+	double dLat = (latitude - other.getLat())*(M_PI / 180.0);
+	double dLon = (longitude - other.getLon())*(M_PI / 180.0);
+	double a = sin(dLat / 2.0)*sin(dLat / 2.0) + cos(latitude*(M_PI / 180.0))*cos(other.getLat()*(M_PI / 180.0))*sin(dLon / 2.0)*sin(dLon / 2.0);
+	double c = 2.0 * atan2(sqrt(a), sqrt(1.0 - a));
+	return radius * c;
 }
 
 GeographicCoordinate geoCoord(double latitude, double longitude)
