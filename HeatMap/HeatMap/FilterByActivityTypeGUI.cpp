924599dff7c011ce1b096cbb4bc167c66c5265a4
Austin <12504656+Austin-Scott@users.noreply.github.com>
2019-01-03 12:01:31 -0600

    Filter by activity type subgroup

--- HeatMap/HeatMap/FilterByActivityTypeGUI.cpp
+++ HeatMap/HeatMap/FilterByActivityTypeGUI.cpp
@@ -9,6 +9,23 @@ void FilterByActivityTypeGUI::setConfig(HeatMapConfiguration * config, form* par
 FilterByActivityTypeGUI::FilterByActivityTypeGUI(form & frm) : group(frm)
 {
 	caption("Filter Activity Type");
+	layout.div("<><vert weight=95% <><filterUnknown><filterRunning><filterCycling><filterWalking><filterSwimming><><<saveChangesButton><discardChangesButton>><>><>");
+	filterUnknown.caption("Filter activities of Unknown type");
+	layout["filterUnknown"] << filterUnknown;
+	filterRunning.caption("Filter activities of Runnning type");
+	layout["filterRunning"] << filterRunning;
+	filterCycling.caption("Filter activities of Cycling type");
+	layout["filterCycling"] << filterCycling;
+	filterWalking.caption("Filter activities of Walking type");
+	layout["filterWalking"] << filterWalking;
+	filterSwimming.caption("Filter activities of Swimming type");
+	layout["filterSwimming"] << filterSwimming;
+	saveChangesButton.caption("Accept");
+	layout["saveChangesButton"] << saveChangesButton;
+	discardChangesButton.caption("Discard");
+	layout["discardChangesButton"] << discardChangesButton;
+	layout.collocate();
+
 }
 
 void FilterByActivityTypeGUI::saveChanges()
