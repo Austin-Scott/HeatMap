9b30141a49799b1ed3d72f1217bc2d9f14967b57
Austin <12504656+Austin-Scott@users.noreply.github.com>
2019-01-03 14:07:47 -0600

    Configure renderer subgroup

--- HeatMap/HeatMap/ConfigRendererGUI.cpp
+++ HeatMap/HeatMap/ConfigRendererGUI.cpp
@@ -10,7 +10,41 @@ void ConfigRendererGUI::setConfig(HeatMapConfiguration * config, form* parentFrm
 ConfigRendererGUI::ConfigRendererGUI(form &frm) : group(frm)
 {
 	caption("Configure Renderer");
-	
+	layout.div("<><vert weight=95% <><<labelOne><spinboxOne><labelTwo><spinboxTwo>><useAnitAliasing><<weight=50% labelThree><textboxOne><buttonOne>><<weight=50% labelFour><textboxTwo><buttonTwo>><<weight=50% labelFive><textboxThree><buttonThree>><><<buttonSaveChanges><buttonDiscardChanges>><>><>");
+	labelOne.caption("Width:");
+	layout["labelOne"] << labelOne;
+	labelTwo.caption("Height:");
+	layout["labelTwo"] << labelTwo;
+	labelThree.caption("Background hex color: ");
+	layout["labelThree"] << labelThree;
+	labelFour.caption("Minimum activity hex color: ");
+	layout["labelFour"] << labelFour;
+	labelFive.caption("Maximum activity hex color: ");
+	layout["labelFive"] << labelFive;
+	spinboxOne.range(1, INT_MAX, 1);
+	layout["spinboxOne"] << spinboxOne;
+	spinboxTwo.range(1, INT_MAX, 1);
+	layout["spinboxTwo"] << spinboxTwo;
+	useAnitAliasing.caption("Use Anti-Aliasing (Recommended)");
+	useAnitAliasing.check(true);
+	layout["useAnitAliasing"] << useAnitAliasing;
+	textboxOne.caption("#000000FF");
+	layout["textboxOne"] << textboxOne;
+	textboxTwo.caption("#FF000080");
+	layout["textboxTwo"] << textboxTwo;
+	textboxThree.caption("#FFFFFFFF");
+	layout["textboxThree"] << textboxThree;
+	buttonOne.caption("Choose");
+	layout["buttonOne"] << buttonOne;
+	buttonTwo.caption("Choose");
+	layout["buttonTwo"] << buttonTwo;
+	buttonThree.caption("Choose");
+	layout["buttonThree"] << buttonThree;
+	buttonSaveChanges.caption("Apply");
+	layout["buttonSaveChanges"] << buttonSaveChanges;
+	buttonDiscardChanges.caption("Discard");
+	layout["buttonDiscardChanges"] << buttonDiscardChanges;
+	layout.collocate();
 }
 
 void ConfigRendererGUI::saveChanges()
--- HeatMap/HeatMap/ConfigRendererGUI.h
+++ HeatMap/HeatMap/ConfigRendererGUI.h
@@ -4,6 +4,7 @@
 #include <nana/gui/widgets/button.hpp>
 #include <nana/gui/widgets/spinbox.hpp>
 #include <nana/gui/widgets/checkbox.hpp>
+#include <nana/gui/widgets/textbox.hpp>
 #include <nana/gui/widgets/group.hpp>
 
 #include "HeatMapConfiguration.h"
@@ -16,6 +17,26 @@ private:
 	HeatMapConfiguration* config;
 	bool unsavedChanges;
 	form* parentFrm;
+
+	place layout{ *this };
+
+	label labelOne{ *this };
+	label labelTwo{ *this };
+	label labelThree{ *this };
+	label labelFour{ *this };
+	label labelFive{ *this };
+	spinbox spinboxOne{ *this };
+	spinbox spinboxTwo{ *this };
+	checkbox useAnitAliasing{ *this };
+	textbox textboxOne{ *this };
+	textbox textboxTwo{ *this };
+	textbox textboxThree{ *this };
+	button buttonOne{ *this };
+	button buttonTwo{ *this };
+	button buttonThree{ *this };
+	button buttonSaveChanges{ *this };
+	button buttonDiscardChanges{ *this };
+
 public:
 	void setConfig(HeatMapConfiguration* config, form* parentFrm);
 	ConfigRendererGUI(form& frm);
